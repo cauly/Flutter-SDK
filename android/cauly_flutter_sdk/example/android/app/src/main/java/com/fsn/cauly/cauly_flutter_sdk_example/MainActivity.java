f13503f0a3b2888f71ad28e46fb414bef54ea667
junhojohn <junhojohn@fsn.co.kr>
2023-07-31 15:24:34 +0900

    예제 추가

---
+++ android/cauly_flutter_sdk/example/android/app/src/main/java/com/fsn/cauly/cauly_flutter_sdk_example/MainActivity.java
@@ -0,0 +1,6 @@
+package com.fsn.cauly.cauly_flutter_sdk_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ android/cauly_flutter_sdk/example/lib/main.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+
+import 'package:flutter/services.dart';
+import 'package:cauly_flutter_sdk/cauly_mobile_ads.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  MobileAds.instance.initialize();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  BannerAd? _banner;
+  InterstitialAd? _interstitialAd;
+
+  @override
+  void initState() {
+    super.initState();
+    _createBanner();
+  }
+
+  void _createBanner() {
+    _banner = BannerAd(
+        listener: BannerAdListener(
+          onReceiveAd: (ad) {
+            debugPrint('BannerAdListener onReceiveAd!!!');
+          },
+          onFailedToReceiveAd: (ad, errorCode, errorMessage) {
+            debugPrint('BannerAdListener onFailedToReceiveAd : $errorCode $errorMessage');
+          },
+          onCloseLandingScreen: (ad) {
+            debugPrint('BannerAdListener onCloseLandingScreen!!!');
+          },
+          onShowLandingScreen: (ad) {
+            debugPrint('BannerAdListener onShowLandingScreen!!!');
+          },
+        ),
+        adInfo: const AdInfo("tIw5rIqr", BannerHeightEnum.adaptive, 320, 50))
+      ..load();
+  }
+
+  void _createInterstitialAd() {
+    InterstitialAd.load(
+        adInfo: const AdInfo(
+            "tIw5rIqr",
+            BannerHeightEnum.adaptive,
+            320,
+            50),
+        adLoadCallback: InterstitialAdLoadCallback(
+          onReceiveInterstitialAd: (ad) {
+            debugPrint('InterstitialAd onReceiveInterstitialAd!!!');
+            _interstitialAd = ad;
+            if (_interstitialAd != null) {
+              _interstitialAd!.show();
+              _interstitialAd = null;
+            }
+          },
+          onFailedToReceiveInterstitialAd: ((errorCode, errorMessage) {
+            debugPrint('InterstitialAd onFailedToReceiveInterstitialAd: $errorMessage');
+            _interstitialAd = null;
+          }),
+          onClosedInterstitialAd: (ad) {
+            debugPrint('InterstitialAd onClosedInterstitialAd');
+          },
+          onLeaveInterstitialAd: (ad) {
+            debugPrint('InterstitialAd onLeaveInterstitialAd');
+          },
+        ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Column(
+          children: [
+            Expanded(
+              child: Container(
+                decoration: const BoxDecoration(color: Colors.amber),
+                child: Center(
+                    child: ElevatedButton(
+                        onPressed: () => _showInterstitialAd(),
+                        child: const Text('광고 가자'))),
+              ),
+            ),
+            _banner == null
+                ? Container()
+                : SizedBox(
+              height: _banner!.bannerSizeHeight.toDouble(),
+              child: AdWidget(ad: _banner!),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  _showInterstitialAd() {
+    if (_interstitialAd == null) {
+      _createInterstitialAd();
+    }
+  }
+}
